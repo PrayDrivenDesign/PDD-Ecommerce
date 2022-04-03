89690659eed24d696626ae48a704f192ebdbc3f7
skarltjr <kisa0828@naver.com>
2022-04-03 22:06:23 +0900

    fix : Category & Product-category 빌더 패턴 적용 - ProductCategory에서 product 필드의 경우 항상 함께 사용한다고 생각하여 eager loading

--- product-service/src/main/java/com/msa/domain/Category.java
+++ product-service/src/main/java/com/msa/domain/Category.java
@@ -1,29 +1,38 @@
 package com.msa.domain;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Table(name = "categories")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Table(name = "categories")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
     @Id
     @Column(name = "category_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "category_name",nullable = false)
+    @Column(name = "category_name", nullable = false)
     private String name;
 
+    @CreatedDate
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @Builder
+    public Category(String categoryName) {
+        this.name = categoryName;
+    }
 }
